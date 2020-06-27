81da28a37b4bff28f12a396cd7845d0c7d1e600e
Ian Timothy <scandium@login02.rc.byu.edu>
2020-06-27 14:26:46 -0600

    First commit of the C++ code for solving the ODE.

---
+++ ode.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+
+string dt;
+string N;
+ifstream  ("params.dat");
+double dt=0.5;
+vector<double> t = {0};
+t.push_back(t[0]+dt);
+cout <<t[1];
+getline(dat,str2);
+stringstream(str2)>>dot2;
+stringstream(str)>>dot;
