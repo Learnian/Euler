e2e8512825f7b85b3f03235e1cfc04d73cdd41b3
Ian Timothy <scandium@login02.rc.byu.edu>
2020-06-27 15:26:12 -0600

    Second commit of C++ program to solve ODE with Explicit Euler.

--- ode.cpp
+++ ode.cpp
@@ -7,14 +7,36 @@ using namespace std;
 
 int main()
 {
+    // ofstream ot("output.dat");
+    //string t;
+    //string x;
+    float dt;
+    float N;
+    ifstream p ("params.dat");
+ 
+    vector<float>inputs;
+    float data;
+    string str;
+    int i=0;
+    while( getline( p, str, ' ' ) )
+    {
+         if (i>3){
+         stringstream(str)>>data;
+         inputs.push_back(data);
+          }
+     i++;
+     }
+ 
+    cout<<inputs[0];
+ 
+ 
+ //  double dt=0.5;
+ //    vector<double> t = {0};
+ //    t.push_back(t[0]+dt);
+//  cout <<t[1];
 
-string dt;
-string N;
-ifstream  ("params.dat");
-double dt=0.5;
-vector<double> t = {0};
-t.push_back(t[0]+dt);
-cout <<t[1];
-getline(dat,str2);
-stringstream(str2)>>dot2;
-stringstream(str)>>dot;
+    }
+ 
+
+
+
