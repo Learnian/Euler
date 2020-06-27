c8a6ef6e4085c2942851be78afa0d154f3ff345a
Ian Timothy <scandium@login02.rc.byu.edu>
2020-06-27 17:53:47 -0600

    Here I make the C++ code output the output.dat file

--- ode.cpp
+++ ode.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    // ofstream ot("output.dat");
+    ofstream ot("output.dat");
     //string t;
     //string x;
     float dt;
@@ -27,16 +27,27 @@ int main()
      i++;
      }
  
-dt=inputs[0];
-N=inputs[1];
-vector<float> t;
-t.push_back(0); 
-for(i=0;i<N;i++){
-     t.push_back(t[i]+dt);
-  }
-for(i=0;i<N;i++){
-     cout<<t[i]<<" \n";
-  } 
+   dt=inputs[0];
+   N=inputs[1];
+   vector<float> t;
+   t.push_back(0); 
+   for(i=0;i<N;i++){
+       t.push_back(t[i]+dt);
+   }
+//for(i=0;i<N;i++){
+//     cout<<t[i]<<" \n";
+//  } 
+   float x0=1.0;
+   vector<float> x(N);
+   x[0]=x0;
+   for(i=0;i<(N-1);i++)
+   {
+     x[i+1]=x[i]+(dt*(-3)*(x[i]));
+   }
+   ot<<"t"<<"  "<<"x\n";
+   for(i=0;i<N;i++){
+     ot<<t[i]<<"  "<<x[i]<<" \n";
+   }
 
     }
  
