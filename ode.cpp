cfad514c1bc9f7573d96f744798704d0bac7ca02
Ian Timothy <scandium@login03.rc.byu.edu>
2020-06-29 13:59:54 -0600

    made it write to 7 output files

--- ode.cpp
+++ ode.cpp
@@ -7,7 +7,14 @@ using namespace std;
 
 int main()
 {
-   // ofstream ot("output.dat");
+    ofstream ot0("output_0.dat");
+    ofstream ot1("output_1.dat");
+    ofstream ot2("output_2.dat");
+    ofstream ot3("output_3.dat");
+    ofstream ot4("output_4.dat");
+    ofstream ot5("output_5.dat");
+    ofstream ot6("output_6.dat");
+    
     //string t;
     //string x;
     double dt;
@@ -48,6 +55,70 @@ int main()
          double t_p=t[in];
          t.push_back((t_p+dt));
      }
+    // string out_str=("output");
+    // string ot_sr;
+    // string e_string;
+    // e_string=to_string(e);
+    // ot_sr=out_str+e_string+".dat";
+    // ofstream ot(ot_sr);
+      double x0=1.0;
+      vector<double> x(N_i);
+      x[0]=x0;
+      for(i=0;i<(N_i-1);i++)
+      {
+        x[i+1]=x[i]+(dt*(-3)*(x[i]));
+      }
+      if(e==0){
+      ot0<<"t"<<e<<"  "<<"x"<<e<<"\n";
+      for(i=0;i<N_i;i++){
+        ot0<<t[i]<<"  "<<x[i]<<" \n";
+     }
+ 
+     }
+     else if(e==1){
+         ot1<<"t"<<e<<"  "<<"x"<<e<<"\n";
+         for(i=0;i<N_i;i++){
+           ot1<<t[i]<<"  "<<x[i]<<" \n";
+         }
+
+     }
+     else if(e==2){
+          ot2<<"t"<<e<<"  "<<"x"<<e<<"\n";
+          for(i=0;i<N_i;i++){
+              ot2<<t[i]<<"  "<<x[i]<<" \n";
+          }
+
+     }
+      else if(e==3){
+           ot3<<"t"<<e<<"  "<<"x"<<e<<"\n";
+           for(i=0;i<N_i;i++){
+               ot3<<t[i]<<"  "<<x[i]<<" \n";
+           }
+
+     }
+     else if(e==4){
+            ot4<<"t"<<e<<"  "<<"x"<<e<<"\n";
+            for(i=0;i<N_i;i++){
+                 ot4<<t[i]<<"  "<<x[i]<<" \n";
+            }
+
+     }
+     else if(e==5){
+           ot5<<"t"<<e<<"  "<<"x"<<e<<"\n";
+           for(i=0;i<N_i;i++){
+               ot5<<t[i]<<"  "<<x[i]<<" \n";
+           }
+
+     }
+     else if(e==6){
+         ot6<<"t"<<e<<"  "<<"x"<<e<<"\n";
+         for(i=0;i<N_i;i++){
+            ot6<<t[i]<<"  "<<x[i]<<" \n";
+          } 
+
+     }
+
+   }
 //     if(e==0){
 //      double a= N_i-N;
 //      cout<<dt<<"   dt\n";
@@ -57,11 +128,11 @@ int main()
 //         cout<<t[h]<<"\n";
 //       }
 //     }
-   cout<<t.back()<<"  back  tn  size  "<<"OR"<<t[t.size()-1]<<" of the "<<e<<"\n\n";
-cout<<t.size()<<" = size   N=  "<<N_i<<" of  "<<e<<"\n\n";
-double dt_test=t[1]-t[0];
-cout<<dt_test<<"  dt of   "<<e<<"   dt actual="<<dt<<"\n\n";
-cout<<t[0]<<" =  t0 of  "<<e<<"\n\n";
+//   cout<<t.back()<<"  back  tn  size  "<<"OR"<<t[t.size()-1]<<" of the "<<e<<"\n\n";
+//cout<<t.size()<<" = size   N=  "<<N_i<<" of  "<<e<<"\n\n";
+//double dt_test=t[1]-t[0];
+//cout<<dt_test<<"  dt of   "<<e<<"   dt actual="<<dt<<"\n\n";
+//cout<<t[0]<<" =  t0 of  "<<e<<"\n\n";
 //for(i=0;i<N;i++){
 //     cout<<t[i]<<" \n";
 //  } 
@@ -77,7 +148,7 @@ cout<<t[0]<<" =  t0 of  "<<e<<"\n\n";
 //     ot<<t[i]<<"  "<<x[i]<<" \n";
 //   }
 
-    }
+    
  
 }
 
